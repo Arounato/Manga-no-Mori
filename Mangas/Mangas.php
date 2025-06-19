bf26c3198fa975b68d4d6e74ff72a24c05657d2e
Vallejos Robert Andres <91800andres@gmail.com>
2025-06-19 20:59:37 +0200

    img + div

---
+++ Mangas/Mangas.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Accueil</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <?php require_once ('../headerfooter/header.php') ?>
+            <main id="corp1">
+                <p>texte</p>
+                <p>e</p>
+            </main>
+        <?php require_once ('../headerfooter/footer.php') ?>
+    </body>
+</html>
