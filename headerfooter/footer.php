20f12f9e92415822039f1be7b8357434ab05a74d
Vallejos Robert Andres <91800andres@gmail.com>
2025-09-18 18:12:38 +0200

    restructure

--- headerfooter/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>Salut</p>
-</footer>
--- headerfooter/header.php
+++
@@ -1,8 +0,0 @@
-<header class="header1">
-    <a href="../accueil.php">
-        <img class="image1" src="../img/logo/manganomori/logo_manganomori(1).png" alt="logo_manganomori(1)"/>
-    </a>
-    <a href="../Mangas/Mangas.php">Mangas</a>
-    <a href="../Mangas/Mangas_neufs.php">Mangas neufs</a>
-    <a href="../Mangas/Mangas_occasions.php">Mangas d'occasions</a>
-</header>
